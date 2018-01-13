e6092470b30d0c70bb5812b2e00417c026cdb11d
GuanjieCao <guanking19@126.com>
2018-01-13 21:40:51 +0800

    mine

---
+++ sigil-plugins/OriNoteFormatter/plugin.py
@@ -0,0 +1,8 @@
+﻿#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+from b7d0ffea995505a5e96e283d960cf498 import *
+def run(b8dc94a88902c55c08a5ed2c69940719):
+	return efe203965684ea38759be8ec74218ce8(b8dc94a88902c55c08a5ed2c69940719);
+
+if __name__ == "__main__":
+	sys.exit(b3d217b241a3928b7bce6e052e71f930())
